9a6ff2bc5a06468846ea69d2e2a23f0ed630079d
ReRamp <aidan@detonatorgames.com>
2018-08-22 20:45:08 -0700

    Started Doing Stuff
    Created function that does nothing, created class that does nothing.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveModule.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class SwerveModule {
+    private DcMotor driveMotor;
+    private DcMotor swerveMotor;
+
+    public SwerveModule(DcMotor driveMotor, DcMotor serveMotor, AnalogInput potentiometer)
+    {
+        
+    }
+}
